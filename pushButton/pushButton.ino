4604a129dee1a74a0b788850c66ec5b4d93c5a7e
j.taneichi <j.tane3ma@gmail.com>
2014-08-12 02:55:09 +0900

    ライトのオンオフ

---
+++ pushButton/pushButton.ino
@@ -0,0 +1,27 @@
+// pushButton.ino
+
+const int LED = 12;
+const int BUTTON = 7;
+int val = 0;
+int val_old = 0;
+int state = 0;
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
+}
+
+void loop() {
+  val = digitalRead(BUTTON);
+  if(val == HIGH && state == 0 && val != val_old){
+    digitalWrite(LED, HIGH);
+    delay(5);
+    state = 1;
+  }else if(val == HIGH && state == 1 && val != val_old){
+    digitalWrite(LED, LOW);
+    delay(5);
+    state = 0;
+  }
+  val_old = val;
+}
+
